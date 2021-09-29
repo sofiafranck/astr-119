135ccc8389760848fd47b8bb33ce79acce7d6a48
sofiafranck <91283162+sofiafranck@users.noreply.github.com>
2021-09-29 12:29:52 -0700

    Create variables_and_loops.py
    example program a) from session 2.

---
+++ astr-119-hw-2/variables_and_loops.py
@@ -0,0 +1,26 @@
+import numpy as np #we use numpy for many things
+
+def main():
+
+	i = 0 # i is an integer declared with a number 0
+	n = 10 # n is another integer 
+	x = 119.0 # floating point numbers are declared with a "."
+
+	# we can use numpy to declare arrays quickly 
+	# an array is a collection of numbers where each number is indexed by another number
+
+	# we can use numpy to declare arrays quickly 
+	y = np.zeros(n,dtype=float)
+
+	#we can us for loops to iterate with a variable
+
+	for i in range(n): #i in range [0, n-1]
+		y[i] = 2.0 * float(i) + 1. #set y=2i+1 as floats 
+
+		#we can simply iterate through a variable array
+	for y_element in y:
+		print(y_element)
+
+#execute the main function
+if __name__=="__main__":
+	main()
